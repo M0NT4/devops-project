b037a322833edfeebe255644814c34e9f4cfe672
M0NT4 <montassaramri@insat.u-carthage.tn>
2022-12-09 06:12:02 -0500

    fix error

--- scripts.groovy
+++ scripts.groovy
@@ -9,4 +9,7 @@ def sonarqube_scan(){
     sh "mvn sonar:sonar -Dsonar.login=admin -Dsonar.password=monta"
 
 }
+def nexus_deploy(){
+    sh "mvn clean package -Dmaven.test.skip=true deploy:deploy-file -DgroupId=com.example -DartifactId=devops-project -Dversion=1.0 -DgeneratePom=true -Dpackaging=jar -DrepositoryId=deploymentRepo -Durl=http://localhost:8081/repository/maven-releases/ -Dfile=target/devops-project-1.0.jar"
+}
 return this
